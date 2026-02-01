b1ffebacd90132fd39c41dec1e50b07ffd23f7f6
Haoming <215781354@qq.com>
2026-02-01 02:06:21 -0500

    MonsterA Break Door State

--- Assets/Scripts/Enemy/State/MonsterA/MonsterABreakDoorState.cs
+++ Assets/Scripts/Enemy/State/MonsterA/MonsterABreakDoorState.cs
@@ -11,11 +11,25 @@ namespace GameJam26.Enemy
         {
             context.animationDriver.EnterAttack();
             context.Motor.Stop();
+            context.doorBreakEndTime = context.currentTime + context.Config.doorBreakDuration;
         }
 
         public void Tick(MonsterAContext context, float deltaTime)
         {
+            if (context.currentTime >= context.doorBreakEndTime)
+            {
+                // 避免卡死在这一步
+                context.shouldBreakDoor = false;
+                
+                if (context.currentDoor == null)
+                {
+                    Debug.LogWarning("MonsterABreakDoorState: currentDoor is already null on break end.");
+                    return;
+                }
 
+                context.currentDoor.SetOpen(true);
+                context.currentDoor = null;
+            }
         }
 
         public void OnExit(MonsterAContext context)
